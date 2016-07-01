b082e2dd4c4cf0d86c49e1ba4c341d31d6fd654e
K-S-A <koniev.s.a@gmail.com>
2016-07-01 10:28:15 +0300

    Remove unused modules/specs.

--- lib/modules/file_export.rb
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# Module holds file export functionality.
-###############################################################################
-module FileExport
-  def export_to(file_name)
-    File.write("export/#{file_name}", to_s)
-  end
-end
